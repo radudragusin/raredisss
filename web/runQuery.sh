5050e1aeef176f0ab63887a6449e73f76fdbb4c8
Radu Dragusin <radu@dragusin.ro>
2011-06-02 21:03:33 +0200

    Added web2py RareDisSS application

--- web/runQuery.sh
+++
@@ -1,4 +0,0 @@
-cd disk/code/runQuery
-echo "Running query with arguments:" $@
-java -cp '.:../../lib/indri.jar:../../lib/gson-1.7.1.jar:../../lib/opencsv-2.2.jar' -Djava.library.path='.:../../lib/' RunQuery "$1" "$2" "$3"
-mv ../../results/$3 ~/results/$3
