ccaf616ad56aa63530add5ad107debc6fb0d9030
Radu Dragusin <radu@dragusin.ro>
2011-06-04 00:25:31 +0400

    Updated opencsvlib version

--- step3.sh
+++ step3.sh
@@ -1,4 +1,4 @@
 cd runQuery
 nano retrieval_param.json
-javac -cp '.:../../lib/indri.jar:../../lib/gson-1.7.1.jar:../../lib/opencsv-2.2.jar' RunQuery.java
-java -cp '.:../../lib/indri.jar:../../lib/gson-1.7.1.jar:../../lib/opencsv-2.2.jar' -Djava.library.path='.:../../lib/' RunQuery retrieval_param.json
+javac -cp '.:../../lib/indri.jar:../../lib/gson-1.7.1.jar:../../lib/opencsv-2.3.jar' RunQuery.java
+java -cp '.:../../lib/indri.jar:../../lib/gson-1.7.1.jar:../../lib/opencsv-2.3.jar' -Djava.library.path='.:../../lib/' RunQuery retrieval_param.json
