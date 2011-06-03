f5d1dc3bf8364f24eb04f32c2bcd9977fd981560
Radu Dragusin <radu@dragusin.ro>
2011-06-04 01:49:26 +0400

    Created devel and stable web apps

--- web/RareDisSS_Stable/controllers/default.py
+++ web/RareDisSS_Stable/controllers/default.py
@@ -17,21 +17,21 @@ def index():
     """
     Test powerTable
     """
-    RUNQUERYPATH = "../master/code/runQuery/"
+    
+    class Virtual(object):
+        @virtualsettings(label=T('Source'))
+        def sourcelink(self):
+            return A(self.t_result.f_source,_href=self.t_result.f_url,_target='_blank')
+    
     session.data = []
     session.query = ""
     logfile = open('logs/'+str(response.session_id),'a')
     logfile.write("SESSION: "+str(response.session_id)+"\n")
     logfile.write("TIME: "+time.strftime("%X %x %z")+"\n")
     db.t_result.truncate()
-    db.t_resdis.truncate()
     query = ""
     collection = ""
-    diseases = ""
-    reranking = ""
     timer = ""
-    table = ""
-    distable = ""
     if request.vars.q:
         t0 = time.clock()
         query = request.vars.q
@@ -40,59 +40,28 @@ def index():
         if checked == "on":
             hashstring = hashlib.md5(request.vars.q+"index_raregenet"+str(time.time())).hexdigest()
             collection = "CHECKED"
-            if request.vars.dis == "on" or request.vars.rer == "on":
-                if request.vars.dis == "on":
-                    os.system('sh '+RUNQUERYPATH+'runQueryDiseases.sh "'+request.vars.q+'" index_raregenet '+hashstring)
-                    diseases = "CHECKED"
-                if request.vars.rer == "on":
-                    os.system('sh '+RUNQUERYPATH+'runQueryReranked.sh "'+request.vars.q+'" index_raregenet '+hashstring)
-                    reranking = "CHECKED"
-            else:
-                os.system('sh '+RUNQUERYPATH+'runQuery.sh "'+request.vars.q+'" index_raregenet '+hashstring)
+            os.system('sh runQuery.sh "'+request.vars.q+'" index_raregenet '+hashstring)
         else:
             hashstring = hashlib.md5(request.vars.q+"index_rare"+str(time.time())).hexdigest()
             collection = ""
-            if request.vars.dis == "on" or request.vars.rer == "on":
-                if request.vars.dis == "on":
-                    os.system('sh '+RUNQUERYPATH+'runQueryDiseases.sh "'+request.vars.q+'" index_rare '+hashstring)
-                    diseases = "CHECKED"
-                if request.vars.rer == "on":
-                    os.system('sh '+RUNQUERYPATH+'runQueryReranked.sh "'+request.vars.q+'" index_rare '+hashstring)
-                    reranking = "CHECKED"
-            else:
-                os.system('sh '+RUNQUERYPATH+'runQuery.sh "'+request.vars.q+'" index_rare '+hashstring)
+            os.system('sh runQuery.sh "'+request.vars.q+'" index_rare '+hashstring)
         db.t_result.truncate()
         db.t_result.import_from_csv_file(open(os.getcwd()+'/results/'+hashstring,'r'))
         for row in db().select(db.t_result.ALL):
             session.data.append([row.f_title,row.f_url,row.f_snippet])
         session.hashstring = hashstring
         session.query = query
-        if request.vars.dis == "on":
-            db.t_resdis.truncate()
-            db.t_resdis.import_from_csv_file(open(os.getcwd()+'/results/'+hashstring+'.dis','r'))
+        #print session.data
         timer = time.clock() - t0
         logfile.write("QUERY: "+query+"\n")
         logfile.write("GENETIC CHECKED: "+str(checked)+"\n")
         logfile.write("RESULT FILE: "+hashstring+"\n")
         logfile.write("SECONDS: "+str(timer)+"\n")
         logfile.close()
-        table = createResultTable(db.t_result)
-        if request.vars.dis == "on":
-            #distable = createDiseaseTable(db.t_resdis)
-            distable = SQLTABLE(db().select(db.t_resdis.f_rank,db.t_resdis.f_disease,db.t_resdis.f_freq,db.t_resdis.f_docnos),headers='labels',truncate=80)
-        timer = str(timer)[:5] + " seconds"
-        print timer
-    return locals()
-    
-def createResultTable(data):
-    class Virtual(object):
-        @virtualsettings(label=T('Source'))
-        def sourcelink(self):
-            return A(self.t_result.f_source,_href=self.t_result.f_url,_target='_blank')
-    
+        timer = str(timer) + " seconds"
+    print timer
     table = plugins.powerTable
-    table.datasource = data
-            
+    table.datasource = db.t_result
     table.dtfeatures['sScrollY'] = '100%'
     table.virtualfields = Virtual()
     table.dtfeatures['bSort'] = False
@@ -119,44 +88,23 @@ def createResultTable(data):
     table.dtfeatures['bFilter'] =  False
     table.dtfeatures['bAutoWidth'] =  False
     table.dtfeatures['aoColumns'] = [
+#            { 'sWidth': '50px' },
+#            { 'sWidth': '50px' },
+#            { 'sWidth': '50px' },
+#            { 'sWidth': '500px' },
+#            { 'sWidth': '300px' }
             { 'sWidth': '0%' },
             { 'sWidth': '5.5%' },
             { 'sWidth': '5.5%' },
             { 'sWidth': '55.5%' },
             { 'sWidth': '33.5%' }
         ]
-    return table.create()
-
-def createDiseaseTable(data):
-    distable = plugins.powerTable
-    distable.datasource = data
     
-    distable.dtfeatures['sScrollY'] = '100%'
-    distable.dtfeatures['bSort'] = False
-    distable.dtfeatures['aaSortingFixed'] = [[2,'asc']]
-    distable.dtfeatures['iDisplayLength'] = 20
-    distable.dtfeatures['bPaginate'] =  False
-    distable.dtfeatures['bInfo'] =  False
-    distable.dtfeatures['bFilter'] =  False
+    table = table.create()
+    #print request.vars.q
+    return locals()
     
-    distable.truncate = 60 
-    distable.headers = 'labels'
-    distable.keycolumn = 't_resdis.id'
-    distable.showkeycolumn = False
     
-    distable.columns = ['t_resdis.f_rank',
-                     't_resdis.f_disease',
-                     't_resdis.f_freq',
-                     't_resdis.f_docnos',
-                     ]
-                     
-    return distable.create()
-
-def filterOnString(fs):
-        fs = fs.lower()
-        query = (db.t_result.f_title.lower().contains(fs) | db.t_result.f_snippet.lower().contains(fs) | db.t_result.f_source.lower().contains(fs) ) 
-        return db(query).select()
- 
 def display_details():
     """
     return a custom object to be inserted in detail for table
@@ -172,11 +120,11 @@ def display_details():
     value = int(key.split('.')[2])
     rows = cPickle.load(response.session_file)['data']
     row = rows[value-1]
-
+    
     logfile = open('logs/'+str(response.session_id),'a')
     logfile.write("OPENED DETAILS FOR RANK: "+str(value)+"\n")
     logfile.close()
-    
+        
     details = DIV(B("Title: "))
     details.append(P(str(row[0])))
     details.append(B("URL: "))
@@ -203,12 +151,6 @@ def feedback():
         message="Feedback received. Thank you!"
         return message
     return ""
-
-def filterResults():
-    fword = str(request.vars.values()[0])
-    data = filterOnString(fword)
-    table = createResultTable(data)
-    return str(table) + "<script>addFilterBox('"+str(fword)+"');$('input#fword').focus().val($('input#fword').val());</script>"
 
 def user():
     """
@@ -242,5 +184,6 @@ def call():
     decorate with @services.jsonrpc the functions to expose
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
+    print "forgeting session"
     session.forget()
     return service()
---
+++ web/RareDisSS_Stable/models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'RareDisSS'
+settings.subtitle = 'Rare Diseases decision Support System'
+settings.author = ''
+settings.author_email = ''
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '5352c8de-fd92-4198-bcf4-f09328950375'
+settings.email_server = ''
+settings.email_sender = ''
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
